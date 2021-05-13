af87579556de86a939d54164127549920ed29843
Alexey <aalekseyf2001@mail.ru>
2021-05-13 14:01:50 +0300

    add all files

---
+++ Arcanoid-v2/Paddle.cpp
@@ -0,0 +1,62 @@
+#include "Paddle.h"
+
+Paddle::Paddle(int x, int y) {
+    shape.setPosition(static_cast<float>(x), static_cast<float>(y));
+    shape.setSize({ this->width, this->height });
+    shape.setFillColor(Color::Red);
+    shape.setOrigin(width / 2.f, height / 2.f);
+}
+
+void Paddle::SetDefualtWidth() {
+    this->width = 150.f;
+    shape.setSize({ this->width, this->height });
+    shape.setOrigin(width / 2.f, height / 2.f);
+}
+
+void Paddle::draw(RenderTarget& target, RenderStates state) const {
+    target.draw(this->shape, state);
+}
+
+void Paddle::update(float winWidth) {
+    this->shape.move(this->velocity);
+
+    if (Keyboard::isKeyPressed(Keyboard::Key::Left) && this->left() > 0) {
+        velocity.x = -paddleVelocity;
+    }
+    else if (Keyboard::isKeyPressed(Keyboard::Key::Right) && this->right() < winWidth) {
+        velocity.x = paddleVelocity;
+    }
+    else {
+        velocity.x = 0.f;
+    }
+}
+
+Vector2f Paddle::getVelocity() {
+    return this->velocity;
+}
+
+float Paddle::left() {
+    return this->shape.getPosition().x - shape.getSize().x / 2.f;
+}
+
+float Paddle::right() {
+    return this->shape.getPosition().x + shape.getSize().x / 2.f;
+}
+
+float Paddle::top() {
+    return this->shape.getPosition().y - shape.getSize().y / 2.f;
+}
+
+float Paddle::bottom() {
+    return this->shape.getPosition().y + shape.getSize().y / 2.f;
+}
+
+void Paddle::IncraseWidth() {
+    this->width += 80.f;
+    shape.setSize({ this->width, this->height });
+    shape.setOrigin(width / 2.f, height / 2.f);
+}
+
+Vector2f Paddle::getPosition() {
+    return shape.getPosition();
+}
---
+++ Arcanoid-v2/main.cpp
@@ -0,0 +1,48 @@
+#include "GameController.h"
+#pragma warning(disable:4996)
+
+int main() {
+	GameController game;
+	game.SetBlocksToDefualt();
+
+	while (game.window.isOpen()) {
+		sf::Event event;
+
+		game.window.pollEvent(event);
+		if (event.type == sf::Event::Closed)
+			game.window.close();
+
+		game.window.clear();
+
+		switch (game.GAMESTATE) {
+		case GAMEPLAY:
+			game.RunGame();
+			game.DrawField();
+			break;
+
+		case GAME_OVER:
+			game.SetBlocksToDefualt();
+
+			if (Keyboard::isKeyPressed(Keyboard::Key::Enter)) {
+				game.GAMESTATE = STATES::GAMEPLAY;
+				sleep(milliseconds(150));
+			}
+			game.DrawGameOver();
+			sleep(seconds(0.8f));
+			game.GAMESTATE = STATES::GAMEPLAY;
+			game.score++;
+			break;
+
+		case WINNER:
+			game.SetBlocksToDefualt();
+
+			if (Keyboard::isKeyPressed(Keyboard::Key::Enter)) {
+				game.GAMESTATE = STATES::GAMEPLAY;
+			}
+			game.score = 0;
+			game.DrawField();
+			break;
+		}
+	}
+	return 0;
+}
